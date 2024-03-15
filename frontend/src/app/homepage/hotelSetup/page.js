208bd9fdcd3b5b5e33dbe69594586dd6f38eae29
nunooppereira <nunofilipepereira.03@gmail.com>
2024-03-15 12:53:39 +0000

    New Visual

---
+++ frontend/src/app/homepage/hotelSetup/page.js
@@ -0,0 +1,11 @@
+"use client"
+import Loader from "@/components/Loader/Loader"
+
+const HotelSetup = () => {
+    return (
+        <>
+            <Loader/>
+        </>
+    )
+}
+export default HotelSetup;
