1ea2b14a9041ae3b9ca21af490ad5f9e31ca4e8c
Bruno Moreira <kayunowna@gmail.com>
2024-03-26 14:20:23 +0000

    Fetch properties users

--- frontend/src/app/api/hotel/properties/[id]/route.js
+++ frontend/src/app/api/hotel/properties/[id]/route.js
@@ -4,6 +4,25 @@ import axios from "axios";
 import prisma from "@/lib/prisma"
 
 
+export async function GET(request, context) {
+
+    const { id } = context.params;
+
+    const response = await prisma.properties.findUnique({
+        where: {
+            propertyID: parseInt(id)
+        }
+    })
+
+    if (!response) {
+        return new NextResponse(JSON.stringify({ status: 404 }));
+    }
+
+    prisma.$disconnect()
+
+    return new NextResponse(JSON.stringify({ response, status: 200 }));
+}
+
 /*export async function PATCH(request, context) {
 
     const prisma = new PrismaClient()
