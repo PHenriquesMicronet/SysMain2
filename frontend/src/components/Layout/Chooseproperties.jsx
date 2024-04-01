a31e283d808f0d12f7f94781ff61d88136881ff8
PHenriques36 <pjvhenriques@gmail.com>
2024-04-01 20:42:09 +0100

    Icon e Texto nos cards

--- frontend/src/components/Layout/Chooseproperties.jsx
+++ frontend/src/components/Layout/Chooseproperties.jsx
@@ -5,9 +5,6 @@ import axios from 'axios';
 
 const ChooseOrganization = () => {
     const [isHovered1, setIsHovered1] = useState(false);
-    const [isHovered2, setIsHovered2] = useState(false);
-    const [isHovered3, setIsHovered3] = useState(false);
-    const [isHovered4, setIsHovered4] = useState(false);
     const [properties, setProperties] = useState([]); // State to store fetched properties
 
     const { data: session, status } = useSession();
@@ -19,7 +16,6 @@ const ChooseOrganization = () => {
                     const userID = session.user.id; // Extract user ID from session
                     const res = await axios.get('/api/hotel/users/'+userID+'/properties');
                     setProperties(res.data.response); // Store fetched properties in state
-                    console.log(userId)
                 } catch (error) {
                     console.error("Error fetching data:", error);
                 }
@@ -33,8 +29,8 @@ const ChooseOrganization = () => {
             <p className="text-center text-3xl mt-24 antialiased">Selecione a propriedade que deseja abrir</p>
             <div className="flex justify-center items-center mt-20">
                 <div className="grid grid-cols-2 gap-4">
-                    {properties.map(properties_users => (
-                        <a href="/homepage" className="card-link" key={properties_users.propertyUserID}>
+                    {properties.map((property, index) => (
+                        <a href="/homepage" className="card-link" key={property.propertyUserID}>
                             <div
                                 className={`card mx-auto ${isHovered1 ? 'hover:scale-105 shadow-lg rounded-lg shadow-slate-300' : ''}`}
                                 onMouseEnter={() => setIsHovered1(true)}
@@ -42,9 +38,9 @@ const ChooseOrganization = () => {
                             >
                                 <Card className="w-80 h-80 flex flex-col justify-center items-center">
                                     <CardHeader className="flex flex-col items-center justify-center">
-                                        <p className="text-large text-center mb-10">{properties_users.propertyID}</p>
+                                        <p className="text-large text-center mb-10">Propriedade {property.name}</p>
                                         <Image className="w-52 h-52"
-                                            src={properties_users.userID}
+                                            src="/images/Logo-Login.png"
                                         />
                                     </CardHeader>
                                 </Card>
