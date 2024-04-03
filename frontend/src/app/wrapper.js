6d92006bd07a0417a6a8b479250d25d11c458713
Bruno Moreira <kayunowna@gmail.com>
2024-04-03 10:47:25 +0000

    Middleware and logout when AFK

--- frontend/src/app/wrapper.js
+++ frontend/src/app/wrapper.js
@@ -1,9 +1,23 @@
 // app/providers.tsx
 'use client'
 import { NextUIProvider } from '@nextui-org/react'
+import { useIdle } from "@uidotdev/usehooks";
+import { signOut } from "next-auth/react"
+import { redirect, usePathname } from "next/navigation";
 
 
 export function Providers({ children }) {
+
+    const pathname = usePathname()
+
+    if (pathname != "/login") {
+        const idle = useIdle(15 * 60 * 1000) //15 * 60 * 1000
+        if (idle) {
+            signOut()
+            redirect("/login")
+        }
+    }
+
     return (
         <NextUIProvider>
             {children}
