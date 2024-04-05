4e50cdcc342aed0d77af4744b89d0cbd44f99638
Bruno Moreira <kayunowna@gmail.com>
2024-04-05 10:46:58 +0000

    Fetch all organization properties licenses

---
+++ frontend/src/app/api/hotel/organizations/[id]/properties/licenses/route.js
@@ -0,0 +1,32 @@
+
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import prisma from "@/lib/prisma"
+
+
+export async function GET(request, context) {
+
+    const { id } = context.params;
+
+    const properties = await prisma.properties.findMany({
+        where: {
+            organizationID: parseInt(id)
+        },
+    })
+
+    const response = await prisma.licenses.findMany({
+        where: {
+            propertyID: {
+                in: properties.map(property => property.propertyID)
+            }
+        },
+    })
+
+    if (!response) {
+        return new NextResponse(JSON.stringify({ status: 404 }));
+    }
+
+    prisma.$disconnect()
+
+    return new NextResponse(JSON.stringify({ response, status: 200 }));
+}
