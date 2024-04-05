4e0990f3306fbabcfc241ef3249cf51c9d05b307
Bruno Moreira <kayunowna@gmail.com>
2024-04-05 12:43:17 +0000

    API for create new organizations

--- frontend/src/app/api/hotel/organizations/route.js
+++ frontend/src/app/api/hotel/organizations/route.js
@@ -10,3 +10,30 @@ export async function GET(request) {
 
     return new NextResponse(JSON.stringify({ response, status: 200 }));
 }
+
+export async function PUT(request) {
+
+    try {
+        const { data } = await request.json();
+        const response = await prisma.organizations.create({
+            data: {
+                name: data.name,
+                fiscalNumber: data.fiscalNumber,
+                email: data.email,
+                phoneNumber: data.phoneNumber,
+                address1: data.address1,
+                address2: data.address2,
+                country: data.country,
+                district: data.district,
+                zipCode: data.zipCode
+            }
+        });
+
+        return new NextResponse(JSON.stringify({ response, status: 200 }));
+
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ error: error.message }), { status: 500 });
+    } finally {
+        await prisma.$disconnect();
+    }
+}
