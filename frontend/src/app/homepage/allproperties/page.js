c7a4678018a2b9c5952ebe531d90455c30c02c24
Bruno Moreira <kayunowna@gmail.com>
2024-04-18 15:24:30 +0000

    Fix typo

--- frontend/src/app/homepage/allproperties/page.js
+++ frontend/src/app/homepage/allproperties/page.js
@@ -1,10 +1,10 @@
 "use client"
-import  Allproperties from "@/components/Layout/Pages/AllProperties"
+import Allproperties from "@/components/Layout/Pages/allProperties"
 
 const allproperties = () => {
     return (
         <>
-            <Allproperties/>
+            <Allproperties />
         </>
     )
 }
