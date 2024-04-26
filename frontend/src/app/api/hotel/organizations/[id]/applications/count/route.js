07317c33f76dde197d5444fd837505d457d2bc1c
Bruno Moreira <kayunowna@gmail.com>
2024-04-26 13:47:51 +0000

    Code cleaning

--- frontend/src/app/api/hotel/organizations/[id]/applications/count/route.js
+++ frontend/src/app/api/hotel/organizations/[id]/applications/count/route.js
@@ -24,22 +24,18 @@ export async function GET(request, context) {
 
     const applications = await prisma.applications.findMany();
 
-    // Objeto para mapear os nomes das aplicações pelos IDs
     const applicationNames = {};
     applications.forEach(application => {
         applicationNames[application.id] = application.description;
     });
 
-    // Iterar sobre o array propertiesApplications para calcular a contagem de registros por nome da aplicação
     const countsByName = {};
     propertiesApplications.forEach(item => {
         const { applicationID } = item;
         const applicationName = applicationNames[applicationID];
-        // Se o nome da aplicação já existir no objeto countsByName, incrementar o contador, senão, inicializá-lo com 1
+
         countsByName[applicationName] = (countsByName[applicationName] || 0) + 1;
     });
-
-    console.log(countsByName)
 
     const response = countsByName
 
