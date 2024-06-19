9c53b7cf8cd74fb0c3be2d9b774f4feec21c9469
Bruno Moreira <kayunowna@gmail.com>
2024-06-19 23:26:21 +0100

    Change language working

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,10 +1,14 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getRequestConfig } from 'next-intl/server';
+import { cookies } from 'next/headers'
 
 export default getRequestConfig(async () => {
     // Provide a static locale, fetch a user setting,
     // read from `cookies()`, `headers()`, etc.
-    const locale = 'en';
+
+    const cookie = cookies().get("language")
+
+    const locale = cookie.value;
 
     return {
         locale,
